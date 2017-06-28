3357dc2b83247a7ed22135109af36d0e74cbe56d
AJ <avjmail@gmail.com>
2017-06-28 20:56:22 +0300

    script cleanup

--- synaptic5.js
+++ synaptic5.js
@@ -1,13 +1,4 @@
-console.log('loaded');
-
-// Example
-// Supervised! Means, we know correct outcomes, not search hidden corellations. Not really "real"
-
-// settings - depends on data
-var inputCount = 2; // age, illness?
-var outputCount = 3; // out in hours, days, weeks?
-var learningRate = 0.4; // trial and error? how to set in unsupervised?
-var trainSteps = 1000;
+// completely fiction data
 
 var young = 0; var old = 1;
 var illLight = 0; var illSevere =1;
@@ -15,7 +6,6 @@ var outInHours = [1, 0, 0];
 var outInDays = [0, 1, 0];
 var outInMonths = [0, 0, 1];
 
-// completely fiction data
 var trainingData = [
     { input: [young, illLight], output: outInHours }, // young, not severe illness -> out in hours
     { input: [young, illSevere], output: outInDays }, // young, severe illness -> out in days
@@ -23,25 +13,32 @@ var trainingData = [
     { input: [old, illSevere], output: outInMonths }, // old, severe illness -> out in weeks
 ];
 
-function train(data) {
-    for (var i = 0; i < data.length; i++) {
-        input.activate(data[i]["input"]);
-        output.activate();
-        output.propagate(learningRate, data[i]["output"]);
-    }
-}
-
-function retrain(data) {
-    for (var i = 0; i < trainSteps; i++) {
-        train(_.shuffle(data));
-    }
-}
-
 // create a network
 var input;
 var output;
 
 function learnStayLength(trainingData) {
+    
+    // settings - depends on data
+    var inputCount = 2; // age, illness?
+    var outputCount = 3; // out in hours, days, weeks?
+    var learningRate = 0.4; // trial and error? how to set in unsupervised?
+    var trainSteps = 1000;
+
+    function train(data) {
+        for (var i = 0; i < data.length; i++) {
+            input.activate(data[i]["input"]);
+            output.activate();
+            output.propagate(learningRate, data[i]["output"]);
+        }
+    }
+
+    function retrain(data) {
+        for (var i = 0; i < trainSteps; i++) {
+            train(_.shuffle(data));
+        }
+    }
+
     input = new synaptic.Layer(inputCount);
     output = new synaptic.Layer(outputCount);
     input.project(output); // map inputs to output
